ab01eea2e94b0ade509b1f008092a8e25e065cc6
Forrest Collman <forrest.collman@gmail.com>
2026-05-14 07:55:37 -0700

    initial zarr vectors point cloud single level implementation

--- src/datasource/enabled_frontend_modules.ts
+++ src/datasource/enabled_frontend_modules.ts
@@ -16,3 +16,4 @@ import "#datasource/python/register_default";
 import "#datasource/render/register_default";
 import "#datasource/vtk/register_default";
 import "#datasource/zarr/register_default";
+import "#datasource/zarr-vectors/register_default";
